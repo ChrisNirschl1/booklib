e603d0de45c1c69ed1c9753121ded83d06499635
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-08 15:50:39 -0800

    started react

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+
+
+export const GET_ME = gql`
+//create a GraphQL query to be executed by Apollo Client
+query GET_ME {
+  User {
+      _id
+      username
+      email
+      bookCount
+      savedBooks
+  }
+}
+`;
